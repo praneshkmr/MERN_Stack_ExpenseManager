09dc4010ee470821281f2f546a94f17172a62780
Pranesh Kumar <praneshkmr@gmail.com>
2016-05-10 10:37:08 +0530

    Basic GET Expense apis

--- fullstack_poc/dao/mongo/expenseDao.js
+++ fullstack_poc/dao/mongo/expenseDao.js
@@ -6,12 +6,20 @@ var expenseDAO = function () {
 };
 
 expenseDAO.prototype.addExpense = function(title, amount, date, callback){
-    var expense = new ExpenseModel({
+  var expense = new ExpenseModel({
     title : "First Title",
     amount : 100,
     date : new Date().now
   });
   expense.save(callback);
+};
+
+expenseDAO.prototype.getAllExpense = function(user, callback){
+  ExpenseModel.find({}, callback);
+};
+
+expenseDAO.prototype.getExpense = function(_id, user, callback){
+  ExpenseModel.find({ "_id" : _id }, callback);
 };
 
 module.exports = expenseDAO;
--- fullstack_poc/routes/expenses.js
+++ fullstack_poc/routes/expenses.js
@@ -6,6 +6,36 @@ var expenseDAO = new expenseDAOClass();
 
 /* GET Expenses listing. */
 router.get('/', function(req, res, next) {
+  expenseDAO.getAllExpense("",function (err, expenses) {
+    if (err){ 
+      console.error(err);
+      res.send(err);
+    }
+    else{
+      res.send(expenses);
+    }
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  if(req.params.id) {
+    var expenseId = req.params.id;
+    expenseDAO.getExpense(expenseId,"",function (err, expenses) {
+      if (err){ 
+        console.error(err);
+        res.send(err);
+      }
+      else{
+        res.send(expenses[0]);
+      }
+    });
+  }
+  else{
+    res.status(400).send('Expense ID is required');
+  }
+});
+
+router.post('/', function (req, res, next) {
   var title = "First Title",
     amount = 100,
     date = new Date().now;
@@ -16,7 +46,7 @@ router.get('/', function(req, res, next) {
     }
     else{
       res.send(expense);
-    }
+    };
   });
 });
 
